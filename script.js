ed91d7b2ce25491b42c2b302d7e152d8d033e6bb
yousefelsemeen <yousefelsemeen@gmail.com>
2024-03-08 01:45:12 +0200

    add main section and footer

--- script.js
+++ script.js
@@ -30,3 +30,17 @@ function handleMenu() {
     ulMenu.style.display = "none";
   }
 }
+
+// control slider
+const wrapper = document.querySelector(".sliderWrapper");
+
+const menuItems = document.querySelectorAll(".menuItem");
+
+menuItems.forEach((menuItem, index) => {
+  menuItem.addEventListener("click", () => {
+    wrapper.style.transform = `translateX(-${index * 100}vw)`;
+    const selectedItem = document.querySelector(".selectedItem");
+    selectedItem.classList.remove("selectedItem");
+    menuItem.classList.add("selectedItem");
+  });
+});
