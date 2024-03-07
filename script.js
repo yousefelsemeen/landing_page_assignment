cb0489f8e0bc4f8de0a63bc23ee621cf484413f7
yousefelsemeen <yousefelsemeen@gmail.com>
2024-03-08 01:58:38 +0200

    add prices

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+const products = [
+  {
+    id: 1,
+    name: "Adidas Yeezy Boost 350 V2",
+    price: 220,
+    image: "./imgs/hippie.png",
+  },
+  {
+    id: 2,
+    name: "Nike Air Max 1 'Evergreen Aura'",
+    price: 140,
+    image: "./imgs/air2.png",
+  },
+  {
+    id: 3,
+    name: "Jordan 1 Retro High Dark Mocha",
+    price: 170,
+    image: "./imgs/jordan.png",
+  },
+  {
+    id: 4,
+    name: "Nike Dunk Low SP Champ Colors",
+    price: 100,
+    image: "./imgs/crater2.png",
+  },
+  {
+    id: 5,
+    name: "Adidas Boost 700 V3 Srphym",
+    price: 200,
+    image: "./imgs/hippie.png",
+  },
+  {
+    id: 6,
+    name: "Skechers D'Lites 3.0",
+    price: 70,
+    image: "./imgs/air.png",
+  },
+];
+
 const mediaQuery = window.matchMedia("(max-width: 768px)");
 
 if (mediaQuery.matches) {
